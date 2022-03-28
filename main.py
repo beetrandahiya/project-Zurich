325991cf1e847bd347a632123085ecc0756eaed6
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-28 12:36:47 +0530

    finally a working neuron layer

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-import sys
-import numpy as np
-import matplotlib
+
 
 #test inputs
-inputs=[1.2, 5.2, 3.2]
-weights=[1, 4.2, 2]
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1],
+[0.5, -0.91, 0.26, -0.5],
+[-0.26, -0.27, 0.17, 0.87]]
 
-bias=3
+biases = [2, 3, 0.5]
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+def neuron_output(inputs, weights,bias):
+    return sum(inputs[i] * weights[i] for i in range(len(inputs)))+ bias
+
+def neuron_layer_output(inputs, weights, biases):
+    outputs=[]
+    for i in range(len(biases)):
+        outputs.append(neuron_output(inputs,weights[i],biases[i]))
+
+    return outputs
+
+
+print(neuron_layer_output(inputs, weights, biases))
